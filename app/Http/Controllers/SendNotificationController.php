3d3c49022e8111ca8a3b60e938b49471e8fe30a6
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-06-03 01:16:04 +0530

    SendNotification model controller migration and sendNotificationForm is generated

---
+++ app/Http/Controllers/SendNotificationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SendNotification;
+use Illuminate\Http\Request;
+
+class SendNotificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SendNotification  $sendNotification
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SendNotification $sendNotification)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SendNotification  $sendNotification
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SendNotification $sendNotification)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SendNotification  $sendNotification
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SendNotification $sendNotification)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SendNotification  $sendNotification
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SendNotification $sendNotification)
+    {
+        //
+    }
+}
