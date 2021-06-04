18e6cc2004413c5f8c794b1116b4a5268f1bb67a
NewsonThokchom <43926860+NewsonThokchom@users.noreply.github.com>
2021-06-05 01:10:27 +0530

    generate queue:table, migrate job table and implement Queue in TaskEventListener

--- app/Listeners/TaskEventListener.php
+++ app/Listeners/TaskEventListener.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 use App\Notifications\SendNotification as NotificationsSendNotification;
 
-class TaskEventListener
+class TaskEventListener implements ShouldQueue
 {
     /**
      * Create the event listener.
